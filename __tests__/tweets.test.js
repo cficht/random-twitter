29ce12c51ae93fd8e51e0c1458c7b9eb854eab78
cficht <chris.ficht@gmail.com>
2020-03-24 13:00:35 -0700

    made and tested tweets post route

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Tweet = require('../lib/models/Tweet');
+
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -17,4 +19,19 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('posts a tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({ handle: 'Chris', text: 'Awesome stuff!' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: 'Chris', 
+          text: 'Awesome stuff!',
+          __v: 0
+        });
+      });
+  });
+
 });
